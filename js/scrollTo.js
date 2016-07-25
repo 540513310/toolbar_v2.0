6059f2bec89e6395256b1d4d750f57f594938bde
540513310 <15899685505@163.com>
2016-07-25 23:14:35 +0800

    封装scrollTo模块

---
+++ js/scrollTo.js
@@ -0,0 +1,29 @@
+define(['jquery'],function ($) {
+	function ScrollTo(opts){
+		//$.extend():如果用户传递了参数opts，则用户的参数覆盖默认的参数
+		//如果用户没有传参，则用默认的参数ScrollTo.DEFAULTS
+		//$.extend()的第二个参数为默认提供的参数，第三个为给定的参数，第一个参数为，生成的新对象
+
+		this.opts=$.extend({},ScrollTo.DEFAULTS,opts);
+
+	}
+
+	ScrollTo.prototype.move = function() {
+		$('html,body').animate({
+			ScrollTop:this.opts.dest
+		},this.opts.speed);
+
+	};
+
+	ScrollTo.prototype.go=function(){
+		$('html,body').ScrollTop(this.opts.dest);
+
+	};
+
+	//给函数ScrollTo添加一个属性，用来存放默认的运动属性
+	ScrollTo.DEFAULTS={
+		dest:0,		//运动到哪里（位置）
+		speed:800	//运动的速度
+	};
+
+}) 
